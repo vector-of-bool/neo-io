8220bf20767d28f0386561e22a9352ac206facdd
vector-of-bool <vectorofbool@gmail.com>
2020-09-13 21:09:39 -0600

    Change stream_io_buffers to use dynbuf_io internally

--- src/neo/io/stream/buffers.hpp
+++ src/neo/io/stream/buffers.hpp
@@ -5,9 +5,11 @@
 #include <neo/io/write.hpp>
 
 #include <neo/as_dynamic_buffer.hpp>
+#include <neo/buffer_sink.hpp>
+#include <neo/buffer_source.hpp>
 #include <neo/bytes.hpp>
+#include <neo/dynbuf_io.hpp>
 #include <neo/fwd.hpp>
-#include <neo/io_buffer.hpp>
 #include <neo/ref.hpp>
 #include <neo/shifting_dynamic_buffer.hpp>
 
@@ -23,8 +25,8 @@ public:
     using buffers_type = std::remove_cvref_t<Stream>;
 
 private:
-    wrap_if_reference_t<Stream>  _strm;
-    wrap_if_reference_t<Buffers> _bufs;
+    wrap_if_reference_t<Stream> _strm;
+    dynbuf_io<Buffers>          _bufs_io;
 
 public:
     constexpr stream_io_buffers() = default;
@@ -34,48 +36,42 @@ public:
 
     constexpr stream_io_buffers(Stream&& s, Buffers&& bufs)
         : _strm(NEO_FWD(s))
-        , _bufs(NEO_FWD(bufs)) {}
+        , _bufs_io(NEO_FWD(bufs)) {}
 
     constexpr auto& stream() noexcept { return unref(_strm); }
     constexpr auto& stream() const noexcept { return unref(_strm); }
-    constexpr auto& buffers() noexcept { return unref(_bufs); }
-    constexpr auto& buffers() const noexcept { return unref(_bufs); }
+    constexpr auto& buffers() noexcept { return _bufs_io.storage(); }
+    constexpr auto& buffers() const noexcept { return _bufs_io.storage(); }
 
     constexpr decltype(auto) prepare(std::size_t size) requires(write_stream<stream_type>) {
-        auto ready_size = buffers().size();
-        if (ready_size < size) {
-            dynbuf_safe_grow(buffers(), size - ready_size);
-        }
-        return buffers().data(0, size);
+        return _bufs_io.prepare(size);
     }
 
     constexpr decltype(auto) commit(std::size_t size) requires(write_stream<stream_type>) {
-        auto out_bufs  = buffers().data(0, size);
-        auto write_res = write(stream(), out_bufs);
-        buffers().consume(write_res.bytes_transferred);
+        _bufs_io.commit(size);
+        auto write_res = write(stream(), _bufs_io.next(size));
+        _bufs_io.consume(write_res.bytes_transferred);
     }
 
     constexpr decltype(auto) next(std::size_t size) requires(read_stream<stream_type>) {
-        auto already_size = buffers().size();
-        if (already_size < size) {
-            auto want_grow_n = size - already_size;
-            auto in_bufs     = dynbuf_safe_grow(buffers(), want_grow_n);
+        if (_bufs_io.available() < size) {
+            auto want_read_n = size - _bufs_io.available();
+            auto in_bufs     = _bufs_io.prepare(want_read_n);
             auto read_res    = read(stream(), in_bufs);
-            auto unused_size = buffer_size(in_bufs) - read_res.bytes_transferred;
-            buffers().shrink(unused_size);
+            _bufs_io.commit(read_res.bytes_transferred);
         }
-        return buffers().data(0, buffers().size());
+        return _bufs_io.next(size);
     }
 
     constexpr decltype(auto) consume(std::size_t size) requires(read_stream<stream_type>) {
-        buffers().consume(size);
+        _bufs_io.consume(size);
     }
 };
 
 template <typename Stream>
-stream_io_buffers(Stream &&)->stream_io_buffers<Stream>;
+stream_io_buffers(Stream &&) -> stream_io_buffers<Stream>;
 
 template <typename Stream, typename Buffers>
-stream_io_buffers(Stream&&, Buffers &&)->stream_io_buffers<Stream, Buffers>;
+stream_io_buffers(Stream&&, Buffers &&) -> stream_io_buffers<Stream, Buffers>;
 
 }  // namespace neo
