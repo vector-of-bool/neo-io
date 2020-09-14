02550278547c49c930e9b9e0bf5fc9c4d9530eed
vector-of-bool <vectorofbool@gmail.com>
2020-09-13 21:08:39 -0600

    Update do_io_op for new buffers_consumer

--- src/neo/io/detail/io_op.hpp
+++ src/neo/io/detail/io_op.hpp
@@ -10,24 +10,22 @@ namespace neo {
 namespace detail {
 
 /**
- * Execute some I/O operation `op` until `cond` returns zero`, or `bufs` is
+ * Execute some I/O operation `op` until `cond` returns zero`, or `bufs_io` is
  * drained.
  */
 template <typename Bufs, typename Cond, typename Op, bool UseBufferRanges>
 constexpr auto
 do_io_op(Bufs&& bufs_, Cond&& cond, std::bool_constant<UseBufferRanges>, Op&& op) noexcept {
     // consumer keeps track of advancing through the buffer as we read/write.
-    buffers_consumer bufs{bufs_};
+    using consumer_type
+        = std::conditional_t<UseBufferRanges, buffers_consumer<Bufs&>, buffers_vec_consumer<Bufs&>>;
+    consumer_type bufs_io{bufs_};
 
-    using ResultType = decltype(op(bufs.next(1)));
+    using ResultType = decltype(op(bufs_io.next(1)));
     ResultType res_acc;
     res_acc.bytes_transferred = 0;
 
     while (true) {
-        if (bufs.empty()) {
-            // No more room in the output
-            break;
-        }
         auto tr_before = res_acc.bytes_transferred;
         // Check our completion condition
         const std::size_t req_to_transfer = std::invoke(cond, std::as_const(res_acc));
@@ -35,23 +33,11 @@ do_io_op(Bufs&& bufs_, Cond&& cond, std::bool_constant<UseBufferRanges>, Op&& op
             // The condition wants to stop
             break;
         }
-        if constexpr (UseBufferRanges) {
-            // Don't attempt to transfer more than 10MB at once
-            const std::size_t n_to_transfer
-                = (std::min)(std::size_t(1024 * 1024 * 10), req_to_transfer);
-            // Do a single operation with multiple buffers
-            res_acc += op(bufs.next(n_to_transfer));
-        } else {
-            // The calling Op does not want buffer ranges (is not vectorized),
-            // so just pass it the next contiguous buffer. Clamp the buffer size
-            // to the size requested by the condition function.
-            mutable_buffer next_buf = as_buffer(bufs.next_contiguous(), req_to_transfer);
-            // Run the single op:
-            res_acc += op(next_buf);
-        }
+        auto next_out = bufs_io.next(req_to_transfer);
+        res_acc += op(next_out);
         // Advance the buffer consumer by how much we transfered:
         auto n_transferred = res_acc.bytes_transferred - tr_before;
-        bufs.consume(n_transferred);
+        bufs_io.consume(n_transferred);
         // If we had a failure, return now
         if (res_acc.is_failure()) {
             break;
